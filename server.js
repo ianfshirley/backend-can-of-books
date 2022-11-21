05b77887dcc567de27f646e0839b02736d80a8c6
ianfshirley <ianfshirley88@gmail.com>
2022-11-21 12:19:14 -0800

    updated server.js with mongoose and book route

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+const Book = require('./models/book');
+
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+mongoose.connect(process.env.DATABASE_URL);
+
+app.get('/books', async (req, res) => {
+  const filterQuery = {};
+  if (req.query.title) {
+    filterQuery.location = req.query.title;
+  }
+  const books = await Book.find(filterQuery);
+  res.send(books);
+});
+
 
 app.get('/test', (request, response) => {
 
-  response.send('test request received')
+  response.send('test request received');
 
-})
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
