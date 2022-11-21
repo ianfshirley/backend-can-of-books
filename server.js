72eb63dc2510f921f89ac4fa11feed05a965836e
Angelos <kounavisangelo@gmail.com>
2022-11-21 18:15:09 -0500

    changes on files

--- server.js
+++ server.js
@@ -11,16 +11,23 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 app.use(cors());
 
+//add validation to confirm we are wired up to our mongo DB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
 mongoose.connect(process.env.DATABASE_URL);
 
-app.get('/books', async (req, res) => {
-  const filterQuery = {};
-  if (req.query.title) {
-    filterQuery.location = req.query.title;
-  }
-  const books = await Book.find(filterQuery);
-  res.send(books);
-});
+// app.get('/books', async (req, res) => {
+//   const filterQuery = {};
+//   if (req.query.title) {
+//     filterQuery.location = req.query.title;
+//   }
+//   const books = await Book.find(filterQuery);
+//   res.send(books);
+// });
 
 
 app.get('/test', (request, response) => {
